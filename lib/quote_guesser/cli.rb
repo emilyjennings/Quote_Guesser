718bfa4a978207b6849f0f345537ead2e2c14136
Emily Jennings <emily.jennings.elizabeth@gmail.com>
2018-09-01 18:58:20 -0400

    creating some more files

---
+++ lib/quote_guesser/cli.rb
@@ -0,0 +1,12 @@
+class QuoteGuesser::CLI
+
+  def call
+    puts "Random Quote:"
+    give_quote
+  end
+
+  def give_quote
+    puts "I served in the Isreali Navy, and it's not an easy thing. - Adam Neumann"
+  end
+
+end
