e8b4d672d684b189fbe3e24d9c05e8c0533e34b3
Emily Jennings <emily.jennings.elizabeth@gmail.com>
2018-09-01 21:46:46 -0400

    made the main class and method shells, ready to scrape data

--- lib/quote_guesser/cli.rb
+++ lib/quote_guesser/cli.rb
@@ -1,12 +1,48 @@
 class QuoteGuesser::CLI
+  attr_accessor :author
 
   def call
     puts "Random Quote:"
     give_quote
+    user_guess
   end
 
   def give_quote
-    puts "I served in the Isreali Navy, and it's not an easy thing. - Adam Neumann"
+    #scrapes https://www.brainyquote.com/authors/adam_neumann and https://www.brainyquote.com/authors/rupaul
+    puts "I served in the Isreali Navy, and it's not an easy thing."
+    @author = "Adam Neumann"
+  end
+
+  def user_guess
+    puts "Is this a quote by RuPaul or Adam Neumann? Type '1' for Adam Neumann and type '2' for RuPaul."
+    input = gets.strip
+    if input == "1"
+      puts "Correct!"
+      puts "Would you like another quote? y/n"
+      input = gets.strip
+      if input == "y"
+        call
+      else
+        nil
+      end
+    elsif input == "2"
+      puts "Wrong!"
+      puts "Would you like another quote? y/n"
+      input = gets.strip
+      if input == "y"
+        call
+      else
+        nil
+      end
+    elsif input == "bio"
+      read_bio
+    else
+      puts "Command not recognized."
+    end
+  end
+
+  def read_bio
+    puts "Here's a bio about the author:"
   end
 
 end
